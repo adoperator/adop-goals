98c1722a1a0ff3179967689f2faf4c8bbd53985a
ShutovAndrey <a.shutov@klientiks.ru>
2021-10-29 13:31:35 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import AdopEvent from "./AdopEvent.js";
+import Cookies from "js-cookie";
 
-window.AdopEvent = function (params) {
-  return new AdopEvent(params);
+var reg = /clickid=([^&=]+)/.exec(window.location.search);
+// reg = [1, 2];
+if (reg) {
+  Cookies.set("ADOPCID", reg[1]);
+}
+window.AdopEvent = function (campaignId, name, value = null, count = null) {
+  var clickid = Cookies.get("ADOPCID");
+  if (!clickid) {
+    return false;
+  }
+  var img = document.createElement("img");
+  //   img.style.width = "170px";
+  //   img.style.height = "170px";
+  //   img.style.backgroundColor = "green";
+  img.style.width = "1px";
+  img.style.height = "1px";
+  img.style.display = "none";
+  var query = `campaign_id=${campaignId}&name=${name}&clickid=${clickid}`;
+  if (value) query += `&value=${value}`;
+  if (count) query += `&count=${count}`;
+  img.src = `https://newService?${query}`;
+
+  document.body.append(img);
 };
