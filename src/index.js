3013e073cd2e29d2b8b4a221314b134712960d3c
ShutovAndrey <a.shutov@klientiks.ru>
2021-11-10 14:47:07 +0300

    upd goalId

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import Cookies from "js-cookie"
+import Cookies from 'js-cookie'
 
 const clickid = /clickid=([^&=]+)/.exec(window.location.search)
 
 if (clickid) {
-  Cookies.set("ADOPCID", clickid[1], { expires: 365 })
+  Cookies.set('ADOPCID', clickid[1], { expires: 365 })
 }
 
-window.AdopEvent = function (campaignId, name, value = null, count = null) {
-  const clickid = Cookies.get("ADOPCID")
+window.AdopEvent = function (goalId, value = null, count = null) {
+  const clickid = Cookies.get('ADOPCID')
 
   if (!clickid) {
     return false
   }
 
-  const img = document.createElement("img")
-  img.style.width = "1px"
-  img.style.height = "1px"
-  img.style.display = "none"
+  const img = document.createElement('img')
+  img.style.width = '1px'
+  img.style.height = '1px'
+  img.style.display = 'none'
 
-  let query = `campaign_id=${campaignId}&name=${name}&clickid=${clickid}`
+  let query = `goal_id=${goalId}&clickid=${clickid}`
 
   if (value) query += `&value=${value}`
   if (count) query += `&count=${count}`
